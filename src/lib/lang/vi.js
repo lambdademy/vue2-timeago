d92636a15bd49b0cadf665dd4be5b68a0e3e4803
LambdaDemy <hong@cloudjetkpi.com>
2019-04-21 20:58:16 +0700

    Create vi.js

---
+++ src/lib/lang/vi.js
@@ -0,0 +1,16 @@
+export default {
+  short: {
+    now: 'mới đây',
+    sec:'giây',
+    min: 'phút',
+    hour: 'giờ',
+    day: 'ngày',
+  },
+  long: {
+    now: 'mới đây',
+    sec:' mới đây',
+    min: ' vài phút trước',
+    hour: ' vài giờ trước',
+    day: ' vài ngày trước',
+  }
+}
