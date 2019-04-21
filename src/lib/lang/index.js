0b3cbd6128e0058f13d00c75766f13fcda5e5583
LambdaDemy <hong@cloudjetkpi.com>
2019-04-21 20:57:01 +0700

    Update index.js

--- src/lib/lang/index.js
+++ src/lib/lang/index.js
@@ -2,6 +2,7 @@ import zh_TW from './zh_TW';
 import zh_CN from './zh_CN';
 import en from './en';
 import jp from './jp';
+import vi from './vi';
 
 
 export default{
@@ -9,4 +10,5 @@ export default{
   zh_CN,
   en,
   jp,
+  vi
 }
